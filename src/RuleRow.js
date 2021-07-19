3c00e39628a92be6668b369cbe36dd534578dab1
Buentello <buentellopc@gmail.com>
2021-07-18 19:34:35 -0500

    disallow value 0 from scoring line

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
-    let state = this.props.score ? 'disabled' : 'active'
-    console.log(this.props.score ? true : false);
-    return state === 'active' ? (
-      <tr className={`RuleRow RuleRow-${state}`} onClick={this.props.doScore} >
+    // this fix (!= null) is key to show the value of 0 marked
+    let state = this.props.score != null ? "disabled" : "active";
+    // console.log("is it null", this.props.score != null);
+    let decide = this.props.score != null ? () => "" : this.props.doScore;
+    return (
+      <tr className={`RuleRow RuleRow-${state}`} onClick={decide}>
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
-    ) : <tr className={`RuleRow RuleRow-${state}`}  >
-    <td className="RuleRow-name">{this.props.name}</td>
-    <td className="RuleRow-score">{this.props.score}</td>
-  </tr>
+    );
   }
 }
 
