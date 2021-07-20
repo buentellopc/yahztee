2b3f89aa646255b81248146ceed1723bfcb561c6
Buentello <buentellopc@gmail.com>
2021-07-20 17:36:17 -0500

    add descriptions to each row

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,14 +3,16 @@ import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
-    // this fix (!= null) is key to show the value of 0 marked
+    // this fix (!= null) is key to show the value of 0 marked with CSS
     let state = this.props.score != null ? "disabled" : "active";
     // console.log("is it null", this.props.score != null);
     let decide = this.props.score != null ? () => "" : this.props.doScore;
+    let score =
+      this.props.score != null ? this.props.score : this.props.description;
     return (
       <tr className={`RuleRow RuleRow-${state}`} onClick={decide}>
         <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
     );
   }
